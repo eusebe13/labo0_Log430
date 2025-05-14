bf27002af4a478144b9614977fab90ad5b0b51ed
eusebe13 <hangikisoni.eusebe@gmail.com>
2025-05-14 14:35:51 +0000

    2 tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Composant App', () => {
+  test('input accepte une saisie utilisateur', () => {
+    render(<App />);
+    const input = screen.getByTestId('name-input');
+
+    fireEvent.change(input, { target: { value: 'Bob' } });
+
+    expect(input.value).toBe('Bob');
+  });
+
+  test('le bouton affiche "Hello <nom>" dans le terminal', () => {
+    console.log = jest.fn(); // mock console.log
+
+    render(<App />);
+    const input = screen.getByTestId('name-input');
+    const button = screen.getByTestId('hello-button');
+
+    fireEvent.change(input, { target: { value: 'Alice' } });
+    fireEvent.click(button);
+
+    expect(console.log).toHaveBeenCalledWith('Hello Alice');
+  });
 });
